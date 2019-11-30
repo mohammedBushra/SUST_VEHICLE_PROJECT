db80ff5c0d6c40eb39b666a629c2d340509d9a58
Mohammad <mohammedbushra0100@gmail.com>
2019-11-29 23:53:29 -0800

    final code

--- WadBushra.java
+++ WadBushra.java
@@ -1,4 +1,3 @@
-
 package projectoopinheritance;
 import java.awt.Component;
 import java.io.File;
@@ -11,7 +10,7 @@ import javax.swing.JOptionPane;
 public class ProjectOOPInheritance {
 
     public static void main(String[] args) throws FileNotFoundException, Exception {
-
+  
       Scanner input = new Scanner(System.in);
       while(true){
           int number = Integer.parseInt(JOptionPane.showInputDialog("Enter \n 1-add the data \n 2-Display the data \n 3-Exit \n:"));
@@ -24,10 +23,48 @@ public class ProjectOOPInheritance {
                 if((num >3 )||( num <1)){
                        JOptionPane.showInputDialog("null pointer exception, try again");
                    }
-
+                
                 switch(num){
-                    case 1:{
+                    case 1:sailboatMethod();
+                    break;
+                    case 2:bicycleMethod();
+                    break;
+                    case 3:carMethod();
+                    break;
+                }
+              break;
+              case 2:  
+                  {
+                   int num1 = Integer.parseInt(JOptionPane.showInputDialog("Enter \n 1-display Sailboat \n 2-display Bicycle \n 3-display Car"));
+                   if((num1 >3 )||( num1 <1)){
+                       JOptionPane.showInputDialog("null pointer exception, try again");
+                   }
+                   switch(num1){
+                       case 1:                   displayDatainFile("Sailboat.txt"); break;
+                       case 2:                   displayDatainFile("Bicycle.txt"); break;
+                       case 3:                   displayDatainFile("Car.txt"); break;
+                   
+                   }
+              }
+              break;
+              case 3: System.exit(0);    
+          }
+      
+      }
+        
+    }
+  static void displayDatainFile(String filenme) throws Exception{
+         Scanner in=new Scanner(new File(filenme));
+         String line=null;
+                while(in.hasNextLine()){
+                    line=in.nextLine();
+             Component parentComponent;
+                   JOptionPane.showMessageDialog(null, line);
 
+    }}
+              
+   public static void sailboatMethod(){
+                       
                          Sailboat object = new Sailboat("",0,0);
                          object.setPowerSource("");
                          object.setNumberOfWheels(0);
@@ -43,10 +80,9 @@ public class ProjectOOPInheritance {
                         }catch(IOException e){
                              JOptionPane.showInputDialog(""+e);
                         }
-                              //JOptionPane.showInputDialog("Success...");
-                    }
-                    break;
-                    case 2:{
+                    }           
+              
+public static void bicycleMethod(){
                          Bicycle object = new Bicycle("",0);
                          object.setPowerSource("");
                          object.setNumberOfWheels(0);
@@ -60,10 +96,9 @@ public class ProjectOOPInheritance {
                         }catch(IOException e){
                              JOptionPane.showInputDialog(""+e);
                         }
-                    }
-                    break;
-                    case 3:{
-
+                    } 
+public static void carMethod(){
+                      
                          Car object = new Car("",0,0,"");
                          object.setPowerSource("");
                          object.setNumberOfWheels(0);
@@ -79,84 +114,44 @@ public class ProjectOOPInheritance {
                              JOptionPane.showInputDialog(""+e);
                         }
                     }
-                    break;
-                }
-              break;
-              case 2:
-                  {
-                   int num1 = Integer.parseInt(JOptionPane.showInputDialog("Enter \n 1-display Sailboat \n 2-display Bicycle \n 3-display Car"));
-                   if((num1 >3 )||( num1 <1)){
-                       JOptionPane.showInputDialog("null pointer exception, try again");
-                   }
-                   switch(num1){
-                       case 1:                   displayDatainFile("Sailboat.txt"); break;
-                       case 2:                   displayDatainFile("Bicycle.txt"); break;
-                       case 3:                   displayDatainFile("Car.txt"); break;
-                    //   if(num1!=1&&num1!=2&&num1!=3)
-                      //    System.out.println("no match choise, enter the number again:");
-
-                   }
-              }
-              break;
-              case 3: System.exit(0);
-          }
-
-      }
-
-        // TODO code application logic here
-    }
-  static void displayDatainFile(String filenme) throws Exception{
-         Scanner in=new Scanner(new File(filenme));
-         String line=null;
-                while(in.hasNextLine()){
-                    line=in.nextLine();
-             Component parentComponent;
-
-                    JOptionPane.showInputDialog(line);
-
-    }}
-
-
-
-
 }
 abstract  class Vehicle {
     private static String powerSource;
     private static double numberOfWheels;
     public double price;
-
+    
     public Vehicle(String powerSource,double numberOfWheels,double price){
         this.powerSource=powerSource;
         this.numberOfWheels=numberOfWheels;
         this.price=price;
     }
-
+   
      public Vehicle(String powerSource,double numberOfWheels){
         this.powerSource=powerSource;
         this.numberOfWheels=numberOfWheels;
     }
-
+    
    public void setPowerSource(String s){
        powerSource = JOptionPane.showInputDialog("Enter your power Source :");
        this.powerSource=powerSource;
     }
-
+   
     String getPowerSource(){
       return this.powerSource;
     }
-
-    public void setNumberOfWheels(double i){
-        numberOfWheels= Double.parseDouble(JOptionPane.showInputDialog("Enter your number Of Wheels :"));
+    
+    public void setNumberOfWheels(double i){      
+        numberOfWheels= Double.parseDouble(JOptionPane.showInputDialog("Enter your number Of Wheels :")); 
         this.numberOfWheels=numberOfWheels
                 ;
     }
-
+     
     double getNumberOfWheels(){
       return numberOfWheels;
     }
-
+    
     public abstract void setPrice(double p);
-
+     
     double getprice()
     {
       return price;
@@ -165,32 +160,31 @@ abstract  class Vehicle {
     return getPowerSource()+"\n"+getNumberOfWheels()+"\n"+getprice();
     }
 }
-
 class Sailboat extends Vehicle {
     private double length;
     Sailboat(String powerSource,double numberOfWheels,double length){
     super(powerSource,numberOfWheels);
     }
-
+       
     public void setPrice(double p){
-       price= Double.parseDouble(JOptionPane.showInputDialog("Enter Sailboat Price : "));
+       price= Double.parseDouble(JOptionPane.showInputDialog("Enter Sailboat Price : ")); 
        this.price=price;
-
+    
     }
 
     public void setLength(double length){
-      length= Double.parseDouble(JOptionPane.showInputDialog("Enter Sailboat length in meter : "));
+      length= Double.parseDouble(JOptionPane.showInputDialog("Enter Sailboat length in meter : ")); 
       this.length=length;
     }
-
+     
     public double getLength(){
         return length;
     }
-
+   
      public String toString(){
         return getPowerSource()+""+getNumberOfWheels()+""+getprice()+""+length;
     }
-
+   
 }
 class Bicycle extends Vehicle {
     Bicycle(String powerSource,double numberOfWheels){
@@ -198,36 +192,36 @@ class Bicycle extends Vehicle {
     }
      @Override
      public void setPrice(double p){
-      price= Double.parseDouble(JOptionPane.showInputDialog("Enter Bicycle Price : "));
+      price= Double.parseDouble(JOptionPane.showInputDialog("Enter Bicycle Price : ")); 
       this.price=price;
     }
-
+     
     public String toString(){
          return getPowerSource()+""+getNumberOfWheels()+""+getprice();
     }
-
+    
 }
 class Car extends Vehicle {
     private String model ;
     public Car(String powerSource,double numberOfWheels,double price,String model){
-       super(powerSource,numberOfWheels,price);
+       super(powerSource,numberOfWheels,price); 
        this.model=model;
     }
-
+        
     public void setPrice(double p){
         price= Double.parseDouble(JOptionPane.showInputDialog("Enter Car Price : "));
         this.price=price;
     }
-
+   
    public void setmodel(String m){
        model= JOptionPane.showInputDialog("Enter Sailboat model in meter : ");
        this.model=model;
-   }
-
+   } 
+   
    public String getmodel(){
        return model;
-   }
-
+   } 
+   
    public String toString(){
        return getPowerSource()+""+getNumberOfWheels()+""+model+""+getprice();//
    }
