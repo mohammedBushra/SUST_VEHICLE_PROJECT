915a8f3e77bc670f0cdf7355cf642408f0f28a5e
Mishaol <58293297+Mishaol@users.noreply.github.com>
2019-11-30 09:29:29 +0200

    Edited code

--- WadBushra.java
+++ WadBushra.java
@@ -11,7 +11,7 @@ import javax.swing.JOptionPane;
 public class ProjectOOPInheritance {
 
     public static void main(String[] args) throws FileNotFoundException, Exception {
-  
+
       Scanner input = new Scanner(System.in);
       while(true){
           int number = Integer.parseInt(JOptionPane.showInputDialog("Enter \n 1-add the data \n 2-Display the data \n 3-Exit \n:"));
@@ -24,10 +24,10 @@ public class ProjectOOPInheritance {
                 if((num >3 )||( num <1)){
                        JOptionPane.showInputDialog("null pointer exception, try again");
                    }
-                
+
                 switch(num){
                     case 1:{
-                       
+
                          Sailboat object = new Sailboat("",0,0);
                          object.setPowerSource("");
                          object.setNumberOfWheels(0);
@@ -63,7 +63,7 @@ public class ProjectOOPInheritance {
                     }
                     break;
                     case 3:{
-                      
+
                          Car object = new Car("",0,0,"");
                          object.setPowerSource("");
                          object.setNumberOfWheels(0);
@@ -82,7 +82,7 @@ public class ProjectOOPInheritance {
                     break;
                 }
               break;
-              case 2:  
+              case 2:
                   {
                    int num1 = Integer.parseInt(JOptionPane.showInputDialog("Enter \n 1-display Sailboat \n 2-display Bicycle \n 3-display Car"));
                    if((num1 >3 )||( num1 <1)){
@@ -94,15 +94,15 @@ public class ProjectOOPInheritance {
                        case 3:                   displayDatainFile("Car.txt"); break;
                     //   if(num1!=1&&num1!=2&&num1!=3)
                       //    System.out.println("no match choise, enter the number again:");
-                    
+
                    }
               }
               break;
-              case 3: System.exit(0);    
+              case 3: System.exit(0);
           }
-      
+
       }
-        
+
         // TODO code application logic here
     }
   static void displayDatainFile(String filenme) throws Exception{
@@ -111,69 +111,52 @@ public class ProjectOOPInheritance {
                 while(in.hasNextLine()){
                     line=in.nextLine();
              Component parentComponent;
-                   
+
                     JOptionPane.showInputDialog(line);
 
     }}
-              
-              
-              
-                   
+
+
+
+
 }
-/*
-Vehicle []array = new Vehicle[3];
-                         array[0] = new Sailboat("",0,0);
-                         array[1] = new Bicycle("",0);
-                         array[2] = new Car("",0,0,"");
-array[0].setPowerSource("");
-array[0].setNumberOfWheels(0);
-array[0].setPrice(0);
-                         String data = array[0].getPowerSource()+"  "+array[0].getNumberOfWheels()+"  "+array[0].getprice();
-                         System.out.println(data);
-
-*/
-
-----------------------------------------------------------------------------------------------------------------------------
-
-package projectoopinheritance;
-import javax.swing.JOptionPane;
-public abstract  class Vehicle {
+abstract  class Vehicle {
     private static String powerSource;
     private static double numberOfWheels;
     public double price;
-    
+
     public Vehicle(String powerSource,double numberOfWheels,double price){
         this.powerSource=powerSource;
         this.numberOfWheels=numberOfWheels;
         this.price=price;
     }
-   
+
      public Vehicle(String powerSource,double numberOfWheels){
         this.powerSource=powerSource;
         this.numberOfWheels=numberOfWheels;
     }
-    
+
    public void setPowerSource(String s){
        powerSource = JOptionPane.showInputDialog("Enter your power Source :");
        this.powerSource=powerSource;
     }
-   
+
     String getPowerSource(){
       return this.powerSource;
     }
-    
-    public void setNumberOfWheels(double i){      
-        numberOfWheels= Double.parseDouble(JOptionPane.showInputDialog("Enter your number Of Wheels :")); 
+
+    public void setNumberOfWheels(double i){
+        numberOfWheels= Double.parseDouble(JOptionPane.showInputDialog("Enter your number Of Wheels :"));
         this.numberOfWheels=numberOfWheels
                 ;
     }
-     
+
     double getNumberOfWheels(){
       return numberOfWheels;
     }
-    
+
     public abstract void setPrice(double p);
-     
+
     double getprice()
     {
       return price;
@@ -183,84 +166,68 @@ public abstract  class Vehicle {
     }
 }
 
---------------------------------------------------------------------------------------------------------------------------
-
-
-package projectoopinheritance;
-import javax.swing.JOptionPane;
 class Sailboat extends Vehicle {
     private double length;
     Sailboat(String powerSource,double numberOfWheels,double length){
     super(powerSource,numberOfWheels);
     }
-       
+
     public void setPrice(double p){
-       price= Double.parseDouble(JOptionPane.showInputDialog("Enter Sailboat Price : ")); 
+       price= Double.parseDouble(JOptionPane.showInputDialog("Enter Sailboat Price : "));
        this.price=price;
-    
+
     }
 
     public void setLength(double length){
-      length= Double.parseDouble(JOptionPane.showInputDialog("Enter Sailboat length in meter : ")); 
+      length= Double.parseDouble(JOptionPane.showInputDialog("Enter Sailboat length in meter : "));
       this.length=length;
     }
-     
+
     public double getLength(){
         return length;
     }
-   
+
      public String toString(){
         return getPowerSource()+""+getNumberOfWheels()+""+getprice()+""+length;
     }
-   
+
 }
-
------------------------------------------------------------------------------------------------------------------------
-
-package projectoopinheritance;
-import javax.swing.JOptionPane;
-public class Bicycle extends Vehicle {
+class Bicycle extends Vehicle {
     Bicycle(String powerSource,double numberOfWheels){
     super(powerSource,numberOfWheels);
     }
      @Override
      public void setPrice(double p){
-      price= Double.parseDouble(JOptionPane.showInputDialog("Enter Bicycle Price : ")); 
+      price= Double.parseDouble(JOptionPane.showInputDialog("Enter Bicycle Price : "));
       this.price=price;
     }
-     
+
     public String toString(){
          return getPowerSource()+""+getNumberOfWheels()+""+getprice();
     }
-    
+
 }
-
------------------------------------------------------------------------------------------------------------------------
-
-
-package projectoopinheritance;
-import javax.swing.JOptionPane;
-public class Car extends Vehicle {
+class Car extends Vehicle {
     private String model ;
     public Car(String powerSource,double numberOfWheels,double price,String model){
-       super(powerSource,numberOfWheels,price); 
+       super(powerSource,numberOfWheels,price);
        this.model=model;
     }
-        
+
     public void setPrice(double p){
         price= Double.parseDouble(JOptionPane.showInputDialog("Enter Car Price : "));
         this.price=price;
     }
-   
+
    public void setmodel(String m){
        model= JOptionPane.showInputDialog("Enter Sailboat model in meter : ");
        this.model=model;
-   } 
-   
+   }
+
    public String getmodel(){
        return model;
-   } 
-   
+   }
+
    public String toString(){
        return getPowerSource()+""+getNumberOfWheels()+""+model+""+getprice();//
    }
